ab785fd59567d98cab9002be6fa5d7296b198865
David Kerr <kerrky@gmail.com>
2024-01-08 20:50:08 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(stringNames, eventNames) {
+  let newArr = [];
+  
+  for(let name of stringNames) {
+    newArr.push(`Thank you, ${name}, for the wonderful ${eventNames} gift!`);
+  }
+  return newArr;
+}
+
+function countDown() {
+  let i = 10;
+
+  while(i >= 0) {
+    i--;
+    console.log(i);
+  }
+}
